2301f225104b616b1f8422adcac565d230fda123
yoseGithub <309015736@qq.com>
2019-07-16 00:35:48 +0800

    section-4.4

--- index.js
+++ index.js
@@ -6,38 +6,38 @@ const router = new Router();
 // 设置前缀，这样下面就不用每次都写/user了
 const userRouter = new Router({prefix: '/user'});
 
+// 内存数据库
+const db = [{name: '李雷'}];
+
 router.get('/', (ctx) => {
     ctx.body = "这是主页"
 });
 
-// post(增)、delete(删)、put(改)、get(查)
 // 获取用户列表
 userRouter.get('/', (ctx) => {
-    // 应该返回查询的数据
-    ctx.body = [
-        {name: '韩梅梅'},
-        {name: '李蕾'}
-    ];
+    ctx.body = db;
+});
+
+// 增加用户
+userRouter.post('/', (ctx) => {
+    db.push(ctx.request.body);
+    ctx.body = ctx.request.body;
 });
 
 // 获取特定用户
 userRouter.get('/:id', (ctx) => {
-    ctx.body = `用户${ctx.params.id}`;
+    ctx.body = db[+ctx.params.id];
 });
 
-// 增加/新建用户
-userRouter.post('/', (ctx) => {
-    // 新建用户，应该返回新建的对象
-    ctx.body = {name: '韩梅梅'};
-});
-
-// 修改用户
-userRouter.put('/', (ctx) => {
-    ctx.body = {name: '涵美眉'};
+// 修改特定用户
+userRouter.put('/:id', (ctx) => {
+    db[+ctx.params.id] = ctx.request.body;
+    ctx.body = ctx.request.body;
 });
 
 // 删除用户
-userRouter.delete('/', (ctx) => {
+userRouter.delete('/:id', (ctx) => {
+    db.splice(+ctx.params.id, 1);
     ctx.status = 204; // 没有内容，但是成功了
 });
 
