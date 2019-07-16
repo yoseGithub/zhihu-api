27de6a925df806fe85a4ebb2a14baff72d4b338e
yose <yosezheng@lexinfintech.com>
2019-07-16 16:35:40 +0800

    section-5.2

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -3,10 +3,19 @@ const db = [{name: '李雷'}];
 
 class UsersCtl {
     find (ctx) {
+        a.b;
         ctx.body = db;
     }
 
-    findId (ctx) {
+    findById (ctx) {
+        if (+ctx.params.id >= db.length) {
+            // ctx.body = '先决条件失败：id 大于数组条件长度';
+            // ctx.status = 412;
+            // return;
+
+            ctx.throw(412, '先决条件失败：id 大于数组条件长度'); // 等价于上面三句话
+        }
+
         ctx.body = db[+ctx.params.id];
     }
 
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,15 +1,15 @@
 const Router = require('koa-router');
-const router = new Router({prefix: '/user'});
-const { find, findId, create, update, delete: del } = require('../controllers/users');
+const router = new Router({prefix: '/users'});
+const { find, findById, create, update, delete: del } = require('../controllers/users');
 
 // 获取用户列表
 router.get('/', find);
 
-// 增加用户
-router.post('/', findId);
-
 // 获取特定用户
-router.get('/:id', create);
+router.get('/:id', findById);
+
+// 增加用户
+router.post('/', create);
 
 // 修改特定用户
 router.put('/:id', update);
