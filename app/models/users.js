b3c940f288bbe6ba8b2138f826c468aa72021184
yoseGithub <309015736@qq.com>
2019-08-30 00:38:07 +0800

    section-9.4

--- app/models/users.js
+++ app/models/users.js
@@ -9,13 +9,14 @@ const userSchema = new Schema({
     avatar_url: { type: String }, // 用户头像
     gender: { type: String, enum: ['male', 'female'], default: 'male', required: true }, // enum 可枚举，性别
     headline: { type: String }, // 一句话简介
-    locations: { type: [{ type: String }] }, // 可枚举的字符串数组，居住地
-    business: { type: String }, // 公司
+    locations: { type: [{ type: String }], select: false }, // 可枚举的字符串数组，居住地
+    business: { type: String, select: false }, // 公司
     employments: {  // 职业经历
         type: [{
             company: { type: String },
             job: { type: String }
-        }]
+        }],
+        select: false
     },
     educations: { // 教育经历
         type: [{
@@ -24,7 +25,8 @@ const userSchema = new Schema({
             diploma: { type: Number, enum: [1, 2, 3, 4, 5] }, // 文凭：初中，高中，大专，本科，本科以上
             entrance_year: { type: Number },
             graduation_year: { type: Number }
-        }]
+        }],
+        select: false
     }
 });
 
